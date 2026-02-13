536fd823b853e08c78f44df629ab16b69b18400d
Playboi In Central <nquockhanhhh1@gmail.com>
2026-02-13 13:09:31 +0700

    Initial Commit

---
+++ CHAT TOGETHER/CHAT_TOGETHERApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHAT_TOGETHERApp.swift
+//  CHAT TOGETHER
+//
+//  Created by Playboi In Central on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CHAT_TOGETHERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CHAT TOGETHER/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CHAT TOGETHER
+//
+//  Created by Playboi In Central on 2/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
